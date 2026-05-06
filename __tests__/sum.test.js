cc712f25a4882e0144aac7f8cba73f74bcdf47c6
ypesok28 <yuvalpk@icloud.com>
2026-05-05 23:07:51 -0700

    fix the unit test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,7 +2,7 @@
 import { sum } from '../code-to-unit-test/sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 test('sum(1, 2) returns 3', () => {
